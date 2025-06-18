59b61aee3a0f150243d0db263badb14ed7ea0054
cheroliv <cheroliv@laposte.net>
2025-06-18 20:45:22 +0200

    Refactor GreetingPlugin to improve task registration and output formatting

--- greeting-plugin/src/main/java/com/cheroliv/plugin/GreetingPlugin.kt
+++ greeting-plugin/src/main/java/com/cheroliv/plugin/GreetingPlugin.kt
@@ -3,14 +3,18 @@ package com.cheroliv.plugin
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 
+@Suppress("unused")
 class GreetingPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.tasks.register(
             "greet",
-            Action { task: Task? ->
-                task!!.doLast(Action { s: Task? -> println("Hello from plugin 'com.cheroliv.plugin.greeting'") })
+            Action { task ->
+                task.doLast(
+                    Action { s ->
+                        "Hello from plugin 'com.cheroliv.plugin.greeting'"
+                            .run(::println)
+                    })
             }
         )
     }
