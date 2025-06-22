83929338d8f72bda3b27ef934eb72319e967941c
cheroliv <cheroliv@laposte.net>
2025-06-22 04:08:31 +0200

    refactor: Rename root project to sample_gradle_plugin-kotlin-dsl for clarity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ pluginManagement.plugins.kotlin("jvm") version "2.1.21"
 
 plugins { this.id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
 
-rootProject.name = "gradle-plugin-in-kotlin"
+rootProject.name = "sample_gradle_plugin-kotlin-dsl"
 include("greeting-plugin")
