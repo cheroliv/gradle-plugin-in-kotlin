5d7ed0f668f196dc0c30a24f6281054999031101
cheroliv <cheroliv@laposte.net>
2025-06-26 02:42:42 +0200

    fix: Update foojay-resolver-convention plugin version to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement.plugins.kotlin("jvm") version "2.1.21"
 
-plugins { this.id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
+plugins { this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 rootProject.name = "sample_gradle_plugin-kotlin-dsl"
 include("greeting-plugin")
